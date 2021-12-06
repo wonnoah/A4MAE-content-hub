b9ad2bd5b91cdab4ec37863bd503f155e49c953a
Noah Wong <wonnoah@amazon.com>
2021-12-06 11:22:12 -0800

    changed fetch credentials to 'omit'

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
 
     const fetchData = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch(url, { credentials: "omit" });
         const json = await response.json();
         console.log(json);
         setData(json);
