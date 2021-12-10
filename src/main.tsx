1f017a337e7bdc61f4ffccaf3a99641cf9277cdd
Noah Wong <wonnoah@amazon.com>
2021-12-10 06:25:16 -0800

    removed focus outline

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,24 @@ import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import "./styles/index.scss";
+import "focus-visible/dist/focus-visible";
+import { Global, css } from "@emotion/react";
+
+const GlobalStyles = css`
+  /*
+    This will hide the focus indicator if the element receives focus    via the mouse,
+    but it will still show up on keyboard focus.
+  */
+  .js-focus-visible :focus:not([data-focus-visible-added]) {
+    outline: none;
+    box-shadow: none;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider resetCSS>
+      <Global styles={GlobalStyles} />
       <App />
     </ChakraProvider>
   </React.StrictMode>,
