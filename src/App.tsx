bddba68504443e60c91d33950a02b7c5b08f9fc1
Noah Wong <wonnoah@amazon.com>
2021-12-11 08:52:42 -0800

    Added section comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
 import { useState, useEffect } from "react";
 import Layout from "./components/Layout";
-import Header from "./components/Header";
 import Hero from "./components/Hero";
-import ContentGrid from "./components/Grid";
-import {
-  Box,
-  Button,
-  Circle,
-  Center,
-  Container,
-  Heading,
-  HStack,
-  Icon,
-  Image,
-  Link,
-  Flex,
-  Grid,
-  GridItem,
-  Tabs,
-  TabList,
-  TabPanels,
-  Tab,
-  TabPanel,
-  Text,
-} from "@chakra-ui/react";
-import { GoChevronRight } from "react-icons/go";
+import ContentGrid from "./components/ContentGrid";
+import { Box, Button, Circle, Center, Container, Heading, Tabs, TabList, TabPanels, Tab, TabPanel, Text } from "@chakra-ui/react";
 
 // Adobe XD: https://xd.adobe.com/view/20ffd87b-1008-4d82-bc49-2e39c3cb2b1d-75ac/
 
 function App() {
   const [all, setAll] = useState([]);
-  console.log("🚀 ~ file: App.tsx ~ line 12 ~ App ~ data", all);
   const [ebooks, setEbooks] = useState([]);
   const [whitepapers, setWhitepapers] = useState([]);
   const [webcasts, setWebcasts] = useState([]);
@@ -92,6 +69,7 @@ function App() {
       <Container maxWidth="1200px" background="#232f3e" p={0}>
         <Tabs variant="enclosed">
           <TabList color="white" justifyContent="space-around" px={3}>
+            {/* Content Production Tab */}
             <Tab
               px={[2, 4]}
               fontSize={["xs", "md"]}
@@ -102,6 +80,9 @@ function App() {
             >
               Content Production
             </Tab>
+
+            {/* Media Supply Chain Tab */}
+
             <Tab
               px={[2, 4]}
               fontSize={["xs", "md"]}
@@ -112,6 +93,8 @@ function App() {
             >
               Media Supply Chain
             </Tab>
+
+            {/* Broadcast Tab */}
             <Tab
               px={[2, 4]}
               fontSize={["xs", "md"]}
@@ -132,6 +115,8 @@ function App() {
             >
               Direct-to-Consumer
             </Tab>
+
+            {/* Data Science & Analytics Tab */}
             <Tab
               px={[2, 4]}
               fontSize={["xs", "md"]}
@@ -145,33 +130,43 @@ function App() {
           </TabList>
 
           <TabPanels>
+            {/* Content Production Content */}
             <TabPanel px={0}>
               <Tabs size="sm" variant="soft-rounded">
                 <TabList flexWrap="wrap" px={4}>
+                  {/* all tag */}
                   <Tab textTransform="uppercase" fontSize="xs" px={2} m={1} borderRadius="lg" color="gray.200" _selected={{ background: "teal.600" }}>
                     <Circle size="25px" color="gray.300" bg="teal.700" mr="2">
                       <Text fontSize="xs"> {all.length}</Text>
                     </Circle>
                     all
                   </Tab>
+
+                  {/* ebooks tag */}
                   <Tab textTransform="uppercase" fontSize="xs" px={2} m={1} borderRadius="lg" color="gray.200" _selected={{ background: "teal.600" }}>
                     <Circle size="25px" color="gray.300" bg="teal.700" mr="2">
                       <Text fontSize="xs"> {ebooks.length}</Text>
                     </Circle>
                     ebooks
                   </Tab>
+
+                  {/* webcasts tag */}
                   <Tab textTransform="uppercase" fontSize="xs" px={2} m={1} borderRadius="lg" color="gray.200" _selected={{ background: "teal.600" }}>
                     <Circle size="25px" color="gray.300" bg="teal.700" mr="2">
                       <Text fontSize="xs"> {webcasts.length}</Text>
                     </Circle>
                     webcasts
                   </Tab>
+
+                  {/* whitepapers tag */}
                   <Tab textTransform="uppercase" fontSize="xs" px={2} m={1} borderRadius="lg" color="gray.200" _selected={{ background: "teal.600" }}>
                     <Circle size="25px" color="gray.300" bg="teal.700" mr="2">
                       <Text fontSize="xs"> {whitepapers.length}</Text>
                     </Circle>
                     whitepapers
                   </Tab>
+
+                  {/* conferences tag */}
                   <Tab textTransform="uppercase" fontSize="xs" px={2} m={1} borderRadius="lg" color="gray.200" _selected={{ background: "teal.600" }}>
                     <Circle size="25px" color="gray.300" bg="teal.700" mr="2">
                       <Text fontSize="xs"> {onlineConferences.length}</Text>
@@ -179,25 +174,37 @@ function App() {
                     conferences
                   </Tab>
                 </TabList>
+
                 <TabPanels>
+                  {/* content production all content */}
                   <TabPanel py={0}>
                     <ContentGrid collection={all} image="all" />
                   </TabPanel>
+
+                  {/* content production ebooks content */}
                   <TabPanel py={0}>
                     <ContentGrid collection={ebooks} image="ebooks" />
                   </TabPanel>
+
+                  {/* content production webcasts content */}
                   <TabPanel py={0}>
                     <ContentGrid collection={webcasts} image="webcasts" />
                   </TabPanel>
+
+                  {/* content production whitepapers content */}
                   <TabPanel py={0}>
                     <ContentGrid collection={whitepapers} image="whitepapers" />
                   </TabPanel>
+
+                  {/* content production conferences content */}
                   <TabPanel py={0}>
                     <ContentGrid collection={onlineConferences} image="onlineConferences" />
                   </TabPanel>
                 </TabPanels>
               </Tabs>
             </TabPanel>
+
+            {/* Media Supply Chain Content */}
             <TabPanel>
               <Center minHeight="400px">
                 <Text fontSize="3xl" color="white">
@@ -205,6 +212,8 @@ function App() {
                 </Text>
               </Center>
             </TabPanel>
+
+            {/* Broadcast Content */}
             <TabPanel>
               <Center minHeight="400px">
                 <Text fontSize="3xl" color="white">
@@ -212,6 +221,8 @@ function App() {
                 </Text>
               </Center>
             </TabPanel>
+
+            {/* D2C & Streaming Content */}
             <TabPanel>
               <Center minHeight="400px">
                 <Text fontSize="3xl" color="white">
@@ -219,6 +230,8 @@ function App() {
                 </Text>
               </Center>
             </TabPanel>
+
+            {/* Data Science & Analytics Content */}
             <TabPanel>
               <Center minHeight="400px">
                 <Text fontSize="3xl" color="white">
